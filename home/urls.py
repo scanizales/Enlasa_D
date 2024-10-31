0413e5c5b8877a017e5110f8837a253da10a3137
Sofía Canizales <clsofi64@gmail.com>
2024-10-31 11:03:11 -0500

    sofia: se agregan interfaces de admin con Django

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     path('profile', views.verPerfil, name = 'verPerfil'),
     path('agregarCliente', views.agregarCliente, name = 'agregarCliente'),
     path('agregarSiniestro', views.agregarSiniestro, name = 'agregarSiniestro'),
-    path('verClientes', views.verClientes, name='verClientes')
+    path('verClientes', views.verClientes, name='verClientes'),
+    path('verPolizas', views.verPolizas, name='verPolizas'),
+    path('verSiniestros', views.verSiniestros, name='verSiniestros'),
+    path('agregarAseguradora', views.agregarAseguradora, name='agregarAseguradora'),
+    path('verAseguradoras', views.verAseguradoras, name='verAseguradoras'),
+    path('agregarTipoSeguro', views.agregarTipoSeguro, name='agregarTipoSeguro'),
+    path('agregarSeguro', views.agregarSeguro, name='agregarSeguro'),
+    path('verTiposSeguros', views.verTiposSeguros, name='verTiposSeguros'),
+    path('verSeguros', views.verSeguros, name='verSeguros'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -14,7 +14,31 @@ def agregarCliente(request):
     return render(request, 'admin/agregarCliente.html')
 
 def agregarSiniestro(request):
-    return render(request, 'admin/agregarSiniestro.html')
+    return render(request, 'admin/agregarSiniestros.html')
 
 def verClientes(request):
     return render(request, 'admin/verClientes.html')
+
+def verPolizas(request):
+    return render(request, 'admin/verPolizas.html')
+
+def verSiniestros(request):
+    return render(request, 'admin/verSiniestros.html')
+
+def agregarAseguradora(request):
+    return render(request, 'admin/agregarAseguradora.html')
+
+def verAseguradoras(request):
+    return render(request, 'admin/verAseguradoras.html')
+
+def agregarTipoSeguro(request):
+    return render(request, 'admin/agregarTipoSeguro.html')
+
+def agregarSeguro(request):
+    return render(request, 'admin/agregarSeguro.html')
+
+def verTiposSeguros(request):
+    return render(request, 'admin/verTiposSeguros.html')
+
+def verSeguros(request):
+    return render(request, 'admin/verSeguros.html')
